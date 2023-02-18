b7601eb8d9803385b59770f2258998489a0d3d21
Free <2331141557@qq.com>
2023-02-18 10:51:28 +0800

    Initial commit

---
+++ src/main/java/com/onlinetool/OnlineToolsApplication.java
@@ -0,0 +1,13 @@
+package com.onlinetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineToolsApplication.class, args);
+    }
+
+}
